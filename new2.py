43b63c02694f90e34d55eaf7a5b1165531929df8
PriyankRaj25 <52851981+PriyankRaj25@users.noreply.github.com>
2019-07-13 14:32:19 +0530

    Update new2.py

--- new2.py
+++ new2.py
@@ -1,5 +1,10 @@
+n=int(input())
 array=list(map(int,input().split()))
 array.sort(reverse=True)
 sum=0
 dict={}
 for i in array:
+  dict[i]=i
+for key,value in dict.items():
+  sum=sum*10+key
+print(sum)
