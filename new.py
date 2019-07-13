b346780479dc341cdfddcae0955648a91047e783
PriyankRaj25 <52851981+PriyankRaj25@users.noreply.github.com>
2019-07-13 14:22:11 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,12 @@
 i=int(input())
 ar=list(map(int,input().split()))
 dict={}
+flag=0
 for i in ar:
+  dict[i]=ar.count(i)
+for key,values in dict.items():
+  if values>1:
+    print(key,end=" ")
+    flag=1
+if flag==0:
+  print("unique")
