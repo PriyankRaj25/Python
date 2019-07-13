014585b1f1dde21ace982db66d9fbfaa5aef0178
PriyankRaj25 <52851981+PriyankRaj25@users.noreply.github.com>
2019-07-13 14:59:39 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,12 +1,15 @@
+
+
 n=int(input())
 array=list(map(int,input().split()))
 dict={}
 flag=0
 for i in array:
   dict[i]=array.count(i)
-for key,values in dict:
+for key,values in dict.items():
   if values==1:
     print(key,end=" ")
     flag=1
 if flag==0:
   print(-1)
+
